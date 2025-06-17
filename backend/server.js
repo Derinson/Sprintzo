7a9488591be431946a9cb35cf77e31b987f37ded
Neydercm <neidercorream@hotmail.com>
2025-06-17 02:45:43 -0500

    HU-024
    Attaching files or images

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const authUser = require("./routes/userRoutes");
 const tablerosRoutes = require("./routes/tablerosRoutes");
 const cardRoutes = require('./routes/cardRoutes');
 const notificacionesRoutes = require('./routes/notificacionesRoutes');
+const path = require('path');
 
 const app = express();
 
@@ -20,11 +21,9 @@ app.use("/userRoutes", authUser); // Ruta de usuarios sigue igual
 app.use("/tablerosRoutes", tablerosRoutes); // Ruta de tableros con autenticación
 app.use('/api/cards', cardRoutes);
 app.use('/api/notificaciones', notificacionesRoutes);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
-
-
-
